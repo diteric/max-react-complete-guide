b6da6e6235205a05a6b67c097280247adb1a8f1d
Yan Li <diteric@gmail.com>
2022-02-23 21:12:06 +0000

    se - init

--- component-basics/src/components/Expenses.js
+++ component-basics/src/components/Expenses.js
@@ -10,6 +10,11 @@ function Expenses(props) {
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}
       ></ExpenseItem>
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      ></ExpenseItem>
     </Card>
   );
 }
