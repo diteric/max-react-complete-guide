cd8513defc746ab64784e31c2fc4d05500ad691c
Yan Li <diteric@gmail.com>
2022-02-24 15:15:24 +0000

    sc - two way bindings on form input

--- state-event/src/components/NexExpense/ExpenseForm.js
+++ state-event/src/components/NexExpense/ExpenseForm.js
@@ -26,6 +26,9 @@ const ExpenseForm = () => {
       date: new Date(enteredDate),
     };
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -33,7 +36,11 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -41,6 +48,7 @@ const ExpenseForm = () => {
             type='mumber'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -50,6 +58,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max=''
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
