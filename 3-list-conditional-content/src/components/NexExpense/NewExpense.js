dcbd15ff0d2c424ece202a65598eca07b205a5d5
Yan Li <diteric@gmail.com>
2022-02-25 15:19:02 +0000

    lcc - make form display conditional

--- 3-list-conditional-content/src/components/NexExpense/NewExpense.js
+++ 3-list-conditional-content/src/components/NexExpense/NewExpense.js
@@ -1,18 +1,35 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+  const [shouldShowNewButton, setShouldShowNewButton] = useState(true);
+
   const saveExpenseDate = (enteredExpenseData) => {
     const expenseDate = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onNewExpenseCreated(expenseDate);
+    hideForm();
+  };
+
+  const hideForm = () => {
+    setShouldShowNewButton(true);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseDate={saveExpenseDate} />
+      {shouldShowNewButton ? (
+        <button onClick={() => setShouldShowNewButton(false)}>
+          Add New Expense
+        </button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseDate={saveExpenseDate}
+          onCancel={() => setShouldShowNewButton(true)}
+        />
+      )}
     </div>
   );
 };
