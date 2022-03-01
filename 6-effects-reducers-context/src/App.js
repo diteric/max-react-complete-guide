26dd811027a6ee7b0c33724e3f34a4110c7703e7
Yan Li <diteric@gmail.com>
2022-03-01 13:22:49 +0000

    6-erc use a customer Context wrapper to make the code clearner.

--- 6-effects-reducers-context/src/App.js
+++ 6-effects-reducers-context/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,41 +6,17 @@ import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/store/auth-context';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginWithLocalStorage = () => {
-    const userLoginLocalStorage = localStorage.getItem('isLoggedIn');
-    console.log(userLoginLocalStorage);
-    if (userLoginLocalStorage === '1') {
-      setIsLoggedIn(true);
-    }
-  };
-
-  useEffect(loginWithLocalStorage, []);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
     // provide the auth context to by wrapping other components inside its provider.
-    <AuthContext.Provider
-      value={{ isLoggedIn: { isLoggedIn }, onLogout: logoutHandler }}
-    >
+    <React.Fragment>
       <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!ctx.isLoggedIn && <Login onLogin={ctx.loginHandler} />}
+        {ctx.isLoggedIn && <Home onLogout={ctx.logoutHandler} />}
       </main>
-    </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
--- 6-effects-reducers-context/src/components/store/auth-context.js
+++ 6-effects-reducers-context/src/components/store/auth-context.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // AuthContext is not a component, but it contains a component.
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
-export default AuthContext;
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginWithLocalStorage = () => {
+    const userLoginLocalStorage = localStorage.getItem('isLoggedIn');
+    if (userLoginLocalStorage === '1') {
+      setIsLoggedIn(true);
+    }
+  };
+
+  useEffect(loginWithLocalStorage, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
