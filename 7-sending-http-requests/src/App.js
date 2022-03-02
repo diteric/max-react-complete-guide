b42e3f423e26b93df84a15683f4d8d4384a2d37d
Yan Li <diteric@gmail.com>
2022-03-02 16:49:32 +0000

    7-apicall post API call example

--- 7-sending-http-requests/src/App.js
+++ 7-sending-http-requests/src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,26 +14,44 @@ function App() {
   const fetchMoviesHandlerAsync = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    const response = await fetch('https://swapi.dev/api/filmsxx');
+    const response = await fetch(
+      'https://react-backend-b1fa8-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+    );
 
     if (!response.ok) {
       setError('response Error');
     } else {
       const data = await response.json();
-      const transformedMovies = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadeMovides = [];
+      for (const key in data) {
+        loadeMovides.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadeMovides);
     }
     setIsLoading(false);
   }, []);
 
-  useEffect(() => fetchMoviesHandlerAsync(), [fetchMoviesHandlerAsync]);
+  // useEffect(() => fetchMoviesHandlerAsync(), [fetchMoviesHandlerAsync]);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      'https://react-backend-b1fa8-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  };
 
   let content = <p></p>;
   if (movies.length > 0) {
@@ -45,6 +64,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandlerAsync}>Fetch Movies</button>
       </section>
