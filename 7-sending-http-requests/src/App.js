cd1e1b785d5efca98ed91ac6cac25acb201560cc
Yan Li <diteric@gmail.com>
2022-03-02 09:59:19 +0000

    7-apicall - apicall state handling, loading, no content, has content

--- 7-sending-http-requests/src/App.js
+++ 7-sending-http-requests/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = () => {
     fetch('https://swapi.dev/api/films')
@@ -27,6 +28,7 @@ function App() {
   // async and await are used together to unwrap the Promise object,
   // It is another way of using the Fetch API
   const fetchMoviesHandlerAsync = async () => {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
 
@@ -40,9 +42,10 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
-  useEffect(fetchMoviesHandler, []);
+  // useEffect(fetchMoviesHandler, []);
 
   return (
     <React.Fragment>
@@ -50,7 +53,9 @@ function App() {
         <button onClick={fetchMoviesHandlerAsync}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No Movie Found.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
