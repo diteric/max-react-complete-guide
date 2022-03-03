2f9928132acac138b28e51c418598d141ff0755e
Yan Li <diteric@gmail.com>
2022-03-03 14:40:20 +0000

    10-redux update store value via react-redux hook 'useDispatch'

--- 10-redux/src/components/Counter.js
+++ 10-redux/src/components/Counter.js
@@ -1,7 +1,8 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
 
   const toggleCounterHandler = () => {};
@@ -9,6 +10,22 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
+      <div>
+        <button
+          onClick={() => {
+            dispatch({ type: 'increment' });
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: 'decrement' });
+          }}
+        >
+          Decrement
+        </button>
+      </div>
       <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
